f75cb55cf982f690f75e12c706838320f7eca05c
Yicheng Wang PC <wang_930109@qq.com>
2020-04-30 15:49:04 +0800

    tiny modification

--- utils/serialization.py
+++ utils/serialization.py
@@ -71,10 +71,7 @@ def save_checkpoint(state, exp_dir, epoch, prefix: str):
 
     # delete previous checkpoints
     files_path = osp.join(save_dir, prefix + '*')
-    try:
-        subprocess.call('rm {0} &> /dev/null'.format(files_path), shell=True)
-    except FileNotFoundError:
-        pass
+    subprocess.call('rm {0}'.format(files_path), shell=True)
     # os.system('rm {0} &>/dev/null'.format(files_path))
 
     # save current checkpoint

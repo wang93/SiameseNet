b3c44fa3183168475aac8375307be39c4f1b1e77
Yicheng Wang PC <wang_930109@qq.com>
2020-05-21 18:44:53 +0800

    fabipc

--- primary_objects_factory/model_with_optimizer_generator.py
+++ primary_objects_factory/model_with_optimizer_generator.py
@@ -62,7 +62,7 @@ def get_model_with_optimizer(opt, id_num=1, naive=False):
         from models.braidnet.braidosnet import EulideanOSNet
         model = EulideanOSNet(feats=opt.feats, num_classes=id_num)
 
-    elif opt.model_name == 'aaosnet':
+    elif opt.model_name == 'aabraidosnet':
         from models.braidnet.braidosnet import AABraidOSNet
         model = AABraidOSNet(feats=opt.feats, fc=fc, score2prob=score2prob)
 

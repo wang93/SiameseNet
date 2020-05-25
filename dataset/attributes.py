a8db851697946a984351db3b3112b22815785967
Yicheng Wang PC <wang_930109@qq.com>
2020-05-25 18:31:25 +0800

    fix a typo

--- dataset/attributes.py
+++ dataset/attributes.py
@@ -2,13 +2,13 @@ import scipy.io as scio
 
 
 def get_market_attributes(set_name='train'):
-    if set_name is not 'train':
+    if set_name is 'gallery':
         raise NotImplementedError
 
     path = 'Market-1501_Attribute/market_attribute.mat'
     data = scio.loadmat(path)
     data = data['market_attribute'][0][0]
-    train_data = data[set_name]
+    data = data[set_name]
 
     attributes = dict()
     fields = ['age', 'backpack', 'bag', 'handbag', 'downblack', 'downblue', 'downbrown', 'downgray', 'downgreen',
@@ -17,7 +17,7 @@ def get_market_attributes(set_name='train'):
               'image_index']
 
     for field in fields:
-        attributes[field] = train_data[field][0][0][0].tolist()
+        attributes[field] = data[field][0][0][0].tolist()
 
     attributes['image_index'] = [str(int(s[0])) for s in attributes['image_index']]
 
--- dataset/samplers.py
+++ dataset/samplers.py
@@ -68,7 +68,7 @@ class RandomIdentitySampler(Sampler):
             replace = False if len(t) >= self.num_instances else True
             t = randchoice(t, size=self.num_instances, replace=replace)
             ret.extend(t)
-        return iter(recdt)
+        return iter(ret)
 
     def __len__(self):
         return self.num_identities * self.num_instances

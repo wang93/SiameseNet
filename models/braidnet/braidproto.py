d2a200af7b811da737302c463e5fee2c2496b2ee
Yicheng Wang PC <wang_930109@qq.com>
2020-05-25 09:55:25 +0800

    amsgrad=False in adam

--- models/braidnet/braidproto.py
+++ models/braidnet/braidproto.py
@@ -70,7 +70,7 @@ class BraidProto(nn.Module, metaclass=ABCMeta):
             optimizer = Adam(param_groups, **default,
                              betas=(0.9, 0.999),
                              eps=1e-8,
-                             amsgrad=True)
+                             amsgrad=False)
         else:
             raise NotImplementedError
 

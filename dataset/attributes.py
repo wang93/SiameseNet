47f90b341acc0ef47614101c96cf101007917821
Yicheng Wang PC <wang_930109@qq.com>
2020-05-25 15:09:51 +0800

    fabipc

--- dataset/attributes.py
+++ dataset/attributes.py
@@ -5,7 +5,7 @@ def get_market_attributes(set_name='train'):
     if set_name is not 'train':
         raise NotImplementedError
 
-    path = '../Market-1501_Attribute/market_attribute.mat'
+    path = 'Market-1501_Attribute/market_attribute.mat'
     data = scio.loadmat(path)
     data = data['market_attribute'][0][0]
     train_data = data[set_name]

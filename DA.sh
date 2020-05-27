0af6687f72e1549c8c643705c83d8d795541ca6f
Yicheng Wang PC <wang_930109@qq.com>
2020-05-27 16:20:34 +0800

    add DA.sh to support DA in multiple settings

---
+++ DA.sh
@@ -0,0 +1,17 @@
+
+gpus=$1
+feats=$2
+model_name=$3
+exp_name=$4
+
+python main_reid.py train  --dataset=market1501 --test_batch=256 --workers=32  \
+       --model_name=$model_name --exp_name=$exp_name --optim=adam  --gpus=$gpus --feats=$feats --check_element_discriminant=train
+
+python main_reid.py train  --dataset=market1501 --test_batch=256 --workers=32  \
+       --model_name=$model_name --exp_name=$exp_name --optim=adam  --gpus=$gpus --feats=$feats --check_element_discriminant=test
+
+python main_reid.py train  --dataset=market1501 --test_batch=256 --workers=32  \
+       --model_name=$model_name --exp_name=$exp_name --optim=adam  --gpus=$gpus --feats=$feats --check_discriminant=train
+
+python main_reid.py train  --dataset=market1501 --test_batch=256 --workers=32  \
+       --model_name=$model_name --exp_name=$exp_name --optim=adam  --gpus=$gpus --feats=$feats --check_discriminant=test

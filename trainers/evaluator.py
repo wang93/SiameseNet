3d94a7fa4c93c7f62223d215e301d7a1d395aa01
Yicheng Wang PC <wang_930109@qq.com>
2020-06-04 19:15:16 +0800

    fix an error

--- trainers/evaluator.py
+++ trainers/evaluator.py
@@ -298,7 +298,7 @@ class ReIDEvaluator:
                 sub_fa, sub_fb = tensor_cuda((sub_fa, sub_fb))
                 scores = fun(sub_fa, sub_fb).cpu().float()
                 score_mat[a_indices, b_indices] = scores
-                # score_mat[b_indices, a_indices] = scores # comment it to avoid duplicated pairs
+                score_mat[b_indices, a_indices] = scores  # comment it to avoid duplicated pairs
 
         weights = self.model.module.get_y_effect()
 

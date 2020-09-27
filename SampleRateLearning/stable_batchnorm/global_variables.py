479f2e672ed92fd9b0e0628c1c3303b5c3303d5a
YIcheng-HP <534579263@qq.com>
2020-09-27 14:52:50 +0800

    parse_target can receive target in Tensor form.

--- SampleRateLearning/stable_batchnorm/global_variables.py
+++ SampleRateLearning/stable_batchnorm/global_variables.py
@@ -1,14 +1,21 @@
 # encoding: utf-8
-
+import torch
 classes_num = 0
 indices = [[]]
 braid_indices = [[]]
 batch_size = None
 
 
-def parse_target(target: list):
-    """each element of target target range from 0. to (classes_num-1)"""
+def parse_target(target):
+    """each element of target ranges from 0. to (classes_num-1)"""
     global classes_num, indices, braid_indices, batch_size
+
+    if isinstance(target, list):
+        pass
+    elif isinstance(target, torch.Tensor):
+        target = target.view(-1).numpy().tolist()
+    else:
+        raise NotImplementedError
 
     indices = [[] for _ in range(classes_num)]
     for i, e in enumerate(target):
@@ -19,22 +26,4 @@ def parse_target(target: list):
     for sub_indices in indices:
         braid_sub_indices = sub_indices + [i + batch_size for i in sub_indices]
         braid_indices.append(braid_sub_indices)
-
-    ####################
-    # indices_0 = []
-    # indices_1 = []
-    # for i, e in enumerate(target):
-    #     if e == 0.:
-    #         indices_0.append(i)
-    #     elif e == 1.:
-    #         indices_1.append(i)
-    #     else:
-    #         raise ValueError
-    #
-    # batch_size = len(target)
-    # braid_indices_0 = indices_0 + [i + batch_size for i in indices_0]
-    # braid_indices_1 = indices_1 + [i + batch_size for i in indices_1]
-    #
-    # indices = [indices_0, indices_1]
-    # braid_indices = [braid_indices_0, braid_indices_1]
 
